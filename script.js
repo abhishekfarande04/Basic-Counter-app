c72ff71691b36de7e2e45589f1eb7c7922fd1912
Abhishek Farande <farandeabhishek04@gmail.com>
2024-01-02 23:13:19 +0530

    .

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const countValue=document.querySelector('#counter'); /// to get the value from HTML doc  
+const countValue=document.querySelector('#counter'); //to get the value from HTML doc  
 
 function increase(){
      let value=parseInt(countValue.innerText); // we use parseint to convert the string value from string to int 
      value=value+1; // main ogic 
      countValue.innerText=value; // set the value back to UI 
-
-
 }
 
 function decrease(){
